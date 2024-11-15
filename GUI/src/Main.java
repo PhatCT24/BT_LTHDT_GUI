f00a4ec1e7fb7832f35355aecdb3d95290e79c1a
PhatCT24 <tienphat0812004@gmail.com>
2024-11-15 16:50:55 +0700

    Delete GUI directory

--- GUI/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Frame;
-public class Main{
-    public static void main(String[] args){
-        Frame f = new Frame("Login");
-        f.setSize(1000,800);
-        f.setVisible(true);
-    }
-}
